820dfc3b605f047e273fe1767be4fe982352a3e5
burugaria7 <burugaria@icloud.com>
2021-03-09 02:03:29 +0900

    フォーム追加して、post関数動くこと確認した

--- makesmf/views.py
+++ makesmf/views.py
@@ -6,13 +6,15 @@ from django.views import View
 class HelloView(View):
     def get(self, request, *args, **kwargs):
         context = {
-            'message': "Hello World! from View!!",
+            'message': "Hello World! from View!!!!!",
         }
         return render(request, 'hello.html', context)
 
     def post(self, request, *args, **kwargs):
         context = {
-            'message': "POST method OK!!",
+            'name': request.POST['name'],
+            'email': request.POST['email'],
+            'message': request.POST['message'],
         }
         return render(request, 'hello.html', context)
 
